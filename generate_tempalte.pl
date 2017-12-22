40866c25de6f053cdded6190719ff590bce3e83f
ZEUGMA1987 <rui.yuan@njit.edu>
2017-12-21 23:15:33 -0500

    Update generate_tempalte.pl

--- generate_tempalte.pl
+++ generate_tempalte.pl
@@ -25,14 +25,17 @@ foreach $sub (@list)
 `cp T2.nii  $output_path/T2_$sub.nii`;  ### copy T2 from each sub and rename them
 
 ####---------- step 1 linear transformation
-  `3dAllineate -source $sub -base $reference -prefix allineate_$sub  -twopass -cost lpa -1Dmatrix_save allineate_$sub.tmp.aff12.1D -autoweight -cmass`;
+  `3dAllineate -source T2_$sub -base $reference -prefix allineate_$sub  -twopass -cost lpa -1Dmatrix_save allineate_$sub.tmp.aff12.1D -autoweight -cmass`;
 
 ####---------- step 1 linear transformation
-  `align_epi_anat.py -dset1 T2_$sub -dset2 T1NL2_76.nii -dset1to2 -giant_move -cost lpc`;
+  #`align_epi_anat.py -dset1 T2_$sub -dset2 T1NL2_76.nii -dset1to2 -giant_move -cost lpc`;
 
 ####---------- step 3 non-linear transformation
   
 `3dQwarp -prefix nonline_allineate_$sub -iwarp -duplo -useweight -blur 0 3 -base $reference -source allineate_$sub `;
+
+
+}
 
 ####---------- step 4 average the inverse transformation 
                                                             #  notice: there are three frames in the inverse matrix image; 
@@ -45,8 +48,14 @@ foreach $sub (@list)
    `3dTstat -prefix mean_INV_2.nii -mean all_INV_2.nii`;
 
    `3dTcat -prefix fin_INV.nii mean_INV_*.nii`; 
+   
+   
 ####---------- step 5 
+foreach $sub (@list)
+{  
+
 `3dNwarpApply -nwarp fin_INV.nii -source nonline_allineate_$sub -prefix inv_nonline_allineate_$sub`; 
+}
 
 ####---------- step 6
 '3dTcat -prefix all_inv_non.nii  inv_nonline_allineate_cp0*'
